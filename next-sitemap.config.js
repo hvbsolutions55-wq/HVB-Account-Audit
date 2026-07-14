bc1b759e232f6fd3b4914c32196b13d076a2ea5a
Iqra Ashraf <iqrashali86121@gmail.com>
2026-07-14 14:34:15 +0500

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  siteUrl: 'https://hvboutsourcing.com',
+  siteUrl: 'https://hvboutsourcing.com', // ✅ YAHAN CHANGE KAREIN
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' }
+    ]
+  },
+  exclude: ['/admin/*', '/private/*'], // Agar koi private pages hain toh
 }
