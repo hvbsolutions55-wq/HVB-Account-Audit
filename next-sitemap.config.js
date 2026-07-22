2f088d22d288c374854e93fd888aec4797073610
Iqra Ashraf <iqrashali86121@gmail.com>
2026-07-22 10:44:18 +0500

    domain cahnges

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://hvboutsourcing.com',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://www.hvboutsourcing.com',
   generateRobotsTxt: true,
   // outDir: 'public',  // by default public hi hota hai
 }
