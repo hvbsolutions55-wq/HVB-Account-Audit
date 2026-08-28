254bd472fb71efc1670a8e9bb2f9deb655b27cae
Iqra Ashraf <iqrashali86121@gmail.com>
2026-08-28 10:58:38 +0500

    change

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -50,7 +50,7 @@ export default async function BlogPage() {
           </div>
         </section>
 
-        <section className="section-space bg-[#f8fafc]">
+        <section className="section-space !pt-0 bg-[#f8fafc]">
           <div className="site-shell">
             <div className="site-shell-inner">
               {!isSanityConfigured ? (
